81c080c57df9d375f0a33315e372eb5a81af9b25
kozimirovdima <kozimirovdima23@gmail.com>
2024-07-28 11:52:29 +0300

    lesson20

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/app/app';
+import { createRoot } from "react-dom/client";
+import App from "./components/app/App";
+import "./style/style.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />,
-  </React.StrictMode>
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
